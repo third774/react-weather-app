d472e3006a18cf4557c333abffce7b435dff7996
Kevin Kipp <kevin.kipp@gmail.com>
2017-05-06 11:15:41 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: __dirname,
+    alias: {
+      
+    }
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+}
