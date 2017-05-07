7b37685fe96ee439a9fd70ae9f0a282e5c2bae91
Kevin Kipp <kevin.kipp@gmail.com>
2017-05-07 08:32:26 -0500

    About about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,15 @@ var React = require('react');
 var About = props => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1>About</h1>
+      <p>Welcome to the React Weather App!</p>
+      <p>This was created following&nbsp; 
+        <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/content">
+          The Complete React Web App Developer Course
+        </a> on Udemy. You can view the source code on my <a href="https://github.com/third774/react-weather-app">
+          GitHub page
+        </a>.
+      </p>
     </div>
   );
 }
