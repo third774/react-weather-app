2eae6b779ff00910c2893eeefc0e79d4a0abc77f
Kevin Kipp <kevin.kipp@gmail.com>
2017-05-06 19:39:29 -0500

    Use gzip on non-dev

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
-
+var compression = require('compression');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
-});
+if (PORT !== 3000) {
+  app.use(compression());
+}
+
+app
+  .use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+  });
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-    console.log(`Express server listening on port ${PORT}`);
+  console.log(`Express server listening on port ${PORT}`);
 });
